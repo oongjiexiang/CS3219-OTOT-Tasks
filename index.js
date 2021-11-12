02396e78c028182ab18f2d5463bff3ef89a6ee8a
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-11-12 16:31:37 +0800

    B completed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const db = require('./db/index')
 const app = express()
 const port = process.env.PORT || 3000;
 
-const classRouter = require('./routes/classRoutes')
 const jobRouter = require('./routes/jobRoutes')
 
 // add middleware
@@ -14,7 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 // add routes
-app.use('/student', classRouter)
 app.use('/job', jobRouter)
 app.listen(port, () => {
     console.log(`Listening at ${port}`)
